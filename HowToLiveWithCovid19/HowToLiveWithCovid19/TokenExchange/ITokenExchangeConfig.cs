2e401ebca8a5b1329d9a54123521d54c3136700f
milindpoint <milindmilind@gmail.com>
2020-06-22 10:12:15 +0530

    First Commit
    first Commit. Working COVID19 BOT.

---
+++ HowToLiveWithCovid19/HowToLiveWithCovid19/TokenExchange/ITokenExchangeConfig.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace HowToLiveWithCovid19.TokenExchange
+{
+    public interface ITokenExchangeConfig
+    {
+        string Provider { get; set; }
+
+        string ConnectionName { get; set; }
+    }
+}
